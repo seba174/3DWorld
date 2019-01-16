30ff68feb4b18c26323115842eddb19de8925da7
Sebastian Safin <seba174@posteo.net>
2019-01-16 23:01:50 +0100

    Add project files.

---
+++ Chess3D/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Chess3D
+{
+    static class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
