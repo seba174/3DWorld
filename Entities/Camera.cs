45371e031ba1742b4ec0e87249286ed865a83207
Sebastian Safin <seba174@posteo.net>
2019-01-19 22:26:00 +0100

    View matrix fix

--- Entities/Camera.cs
+++ Entities/Camera.cs
@@ -5,7 +5,7 @@ namespace Entities
     public class Camera
     {
         public Vector3 Position { get; private set; } = new Vector3(0, 7, 0);
-        public float Pitch { get; } = 0;
+        public float Pitch { get; } = 10;
         public float Yaw { get; } = 0;
         public float Roll { get; } = 0;
 
